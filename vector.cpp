481fb0ab98641d0a1fca3a997cff860fd4127a9c
SaminYasar <md.samin.yasar.saadat@g.bracu.ac.bd>
2021-04-06 04:08:37 +0600

    vector done

---
+++ vector.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> v;
+    v.push_back(40);
+    v.push_back(60);
+    v.push_back(70);
+    v.push_back(80);
+
+    cout << v.size() <<endl;
+    return 0;
+
+}
