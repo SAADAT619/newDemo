289a233b9ba8f2ab1bbcca9606adb8db850a73bb
SaminYasar <md.samin.yasar.saadat@g.bracu.ac.bd>
2021-04-05 23:49:42 +0600

    problem solving

---
+++ week1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t, a,b, sum=0;
+    cin>> t ;
+
+    if(t<=125 && a,b<10){
+        cin >> a >> b;
+        cout << "Case 1: " << a+b << "\n";
+
+        cin >> a >> b;
+        cout << "Case 2: " << a+b << "\n";
+    }
+
+    return 0;
+}
