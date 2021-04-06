631ca67d80c8d0f75b357014e9d1f822e546a40c
SaminYasar <md.samin.yasar.saadat@g.bracu.ac.bd>
2021-04-06 10:09:08 +0600

    again change

--- vec.cpp
+++ vec.cpp
@@ -3,17 +3,31 @@
 using namespace std;
 
 int main(){
-    vector<int> v;
-    v.push_back(10);
-    v.push_back(20);
-    v.push_back(30);
-    v.push_back(40);
-    v.push_back(50);
-    v.push_back(60);
+    // vector<int> v;
+    // v.push_back(10);
+    // v.push_back(20);
+    // v.push_back(30);
+    // v.push_back(40);
+    // v.push_back(50);
+    // v.push_back(60);
 
-    for(int i = 0; i<v.size(); i++){
-        cout << v[i] << " "; 
+    // for(int i = 0; i<v.size(); i++){
+    //     cout << v[i] << " "; 
+    // }
+
+    
+    // //we can use another kind of for loop
+    // for(int x : v){ //That means ekhane sob v er element gula x er moddhe jabe
+    //     cout << x << " ";
+    // }
+    // cout << endl;
+    // cout << v.size() << endl;
+
+
+    //We can use second bracket to show the element in vector
+    vector<int> z{2,3,4,5,6}; 
+    for(int x : z){
+        cout << x << " ";
     }
 
-    cout << v.size() << endl;
 }
